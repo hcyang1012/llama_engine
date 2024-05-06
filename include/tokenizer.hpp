f4b54c0e533f7ab084c4fcd0c70327ce38e9e496
Heecheol Yang <heecheol.yang@outlook.com>
2024-05-07 01:22:46 +0900

    Introduce tokenizer

---
+++ include/tokenizer.hpp
@@ -0,0 +1,71 @@
+/**
+ * @file tokenizer.hpp
+ * @brief Header for the Tokenizer class.
+ * @author Heecheol Yang (heecheol.yang@outlook.com)
+ * @date 2024-05-03
+ */
+
+#pragma once
+
+// C System-Headers
+
+// C++ System-Headers
+#include <string>
+#include <vector>
+
+// Project Headers
+
+// Third-party Headers
+
+namespace llama2 {
+template <typename T> class Tokenizer {
+public:
+  struct TokenIndex {
+    std::string str;
+    size_t id;
+  };
+
+  Tokenizer(const std::string &vocab_file, const size_t vocab_size)
+      : vocab_size_(vocab_size) {
+    load_vocab(vocab_file);
+  }
+
+private:
+  std::vector<std::string> vocab_;
+  std::vector<T> vocab_scores_;
+  std::vector<TokenIndex> sorted_vocab_;
+  size_t vocab_size_;
+  uint32_t max_token_length_;
+  std::vector<std::string> pices_;
+
+  void load_vocab(const std::string &vocab_file) {
+    vocab_.resize(vocab_size_);
+    vocab_scores_.resize(vocab_size_);
+
+    // Initize pieces
+    for (int c = 0; c < 128; c++) {
+      pices_.push_back(std::string(1, c));
+    }
+
+    // Load the vocabulary file
+    std::ifstream if_vocab_file(vocab_file, std::ios::binary);
+    if (!if_vocab_file.is_open()) {
+      throw std::runtime_error("Failed to open the vocabulary file.");
+    }
+
+    // Load the vocabulary
+    if_vocab_file.read(reinterpret_cast<char *>(&max_token_length_),
+                       sizeof(max_token_length_));
+    for (size_t i = 0; i < vocab_size_; i++) {
+      int32_t len;
+      float score;
+      if_vocab_file.read(reinterpret_cast<char *>(&score), sizeof(score));
+      vocab_scores_[i] = score;
+      if_vocab_file.read(reinterpret_cast<char *>(&len), sizeof(len));
+      vocab_[i].resize(len + 1);
+      if_vocab_file.read(&vocab_[i][0], len);
+      vocab_[i][len] = '\0';
+    }
+  }
+};
+} // namespace llama2
--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
+
+
+// C System-Headers
+
+// C++ System-Headers
 #include <cstring>
 #include <iostream>
+
+// Project Headers
 #include <run_state.hpp>
+#include <tokenizer.hpp>
 #include <transformer.hpp>
+
+// Third-party Headers
 
 void error_usage(const std::string &program_name) {
   std::cerr << "Usage: " << program_name << " [options] checkpoint_path\n";
@@ -15,6 +25,8 @@ void error_usage(const std::string &program_name) {
   std::cerr << "  -i <prompt>         Set the prompt string\n";
   std::cerr << "  -m <mode>           Set the mode (default: generate)\n";
   std::cerr << "  -y <system_prompt>  Set the system prompt\n";
+  std::cerr << "  -z <tokenizer_path> Set the tokenizer path (default: "
+               "tokenizer.bin)\n";
   std::exit(1);
 }
 
@@ -23,15 +35,15 @@ int main(int argc, char *argv[]) {
   std::string checkpoint_path;
   std::string tokenizer_path("tokenizer.bin");
   float temperature =
-      1.0f;  // 0.0 = greedy deterministic. 1.0 = original. don't set higher
+      1.0f; // 0.0 = greedy deterministic. 1.0 = original. don't set higher
   float topp =
-      0.9f;  // top-p in nucleus sampling. 1.0 = off. 0.9 works well, but slower
-  int steps = 256;                  // number of steps to run for
-  char *prompt = NULL;              // prompt string
-  unsigned long long rng_seed = 0;  // seed rng with time by default
-  std::string mode("generate");     // mode to run in
+      0.9f; // top-p in nucleus sampling. 1.0 = off. 0.9 works well, but slower
+  int steps = 256;                 // number of steps to run for
+  char *prompt = NULL;             // prompt string
+  unsigned long long rng_seed = 0; // seed rng with time by default
+  std::string mode("generate");    // mode to run in
   char *system_prompt =
-      NULL;  // the (optional) system prompt to use in chat mode
+      NULL; // the (optional) system prompt to use in chat mode
 
   // poor man's C argparse so we can override the defaults above from the
   // command line
@@ -44,13 +56,13 @@ int main(int argc, char *argv[]) {
     // do some basic validation
     if (i + 1 >= argc) {
       error_usage(argv[0]);
-    }  // must have arg after flag
+    } // must have arg after flag
     if (argv[i][0] != '-') {
       error_usage(argv[0]);
-    }  // must start with dash
+    } // must start with dash
     if (strlen(argv[i]) != 2) {
       error_usage(argv[0]);
-    }  // must be -x (one dash, one letter)
+    } // must be -x (one dash, one letter)
     // read in the args
     if (argv[i][1] == 't') {
       temperature = atof(argv[i + 1]);
@@ -66,16 +78,22 @@ int main(int argc, char *argv[]) {
       mode = argv[i + 1];
     } else if (argv[i][1] == 'y') {
       system_prompt = argv[i + 1];
+    } else if (argv[i][1] == 'z') {
+      tokenizer_path = argv[i + 1];
     } else {
       error_usage(argv[0]);
     }
   }
 
   // parameter validation/overrides
-  if (rng_seed <= 0) rng_seed = (unsigned int)time(NULL);
-  if (temperature < 0.0) temperature = 0.0;
-  if (topp < 0.0 || 1.0 < topp) topp = 0.9;
-  if (steps < 0) steps = 0;
+  if (rng_seed <= 0)
+    rng_seed = (unsigned int)time(NULL);
+  if (temperature < 0.0)
+    temperature = 0.0;
+  if (topp < 0.0 || 1.0 < topp)
+    topp = 0.9;
+  if (steps < 0)
+    steps = 0;
 
   // build the Transformer via the model .bin file
   llama2::Transformer<float> transformer(checkpoint_path);
@@ -85,5 +103,8 @@ int main(int argc, char *argv[]) {
 
   steps = std::min(steps, transformer.GetConfig().SeqLen());
 
-    return 0;
+  llama2::Tokenizer<float> tokenizer(tokenizer_path,
+                                     transformer.GetConfig().VocabSize());
+
+  return 0;
 }
