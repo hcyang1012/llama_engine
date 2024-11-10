32ed8c0cc864c46905daf917bc0c089c164d18d1
Heecheol Yang <heecheol.yang@dnotitia.com>
2024-11-11 01:45:41 +0900

    WIP

--- tests/op_test.cpp
+++ tests/op_test.cpp
@@ -5,11 +5,45 @@
 
 #include "op.hpp"
 #include "reference.hpp"
-TEST(OPTest, rmsnorm_test) {
-  const std::vector<float> x = {1.0f, 2.0f, 3.0f, 4.0f};
-  const std::vector<float> weight = {1.0f, 2.0f, 3.0f, 4.0f};
+#include "transformer.hpp"
+#include "weights.hpp"
+class RmsNormTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    // code here will execute just before the test ensues
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(0.0f, 1.0f);
 
-  std::vector<float> expected(4, 0.0f);
-  reference::rmsnorm(expected.data(), x.data(), weight.data(), 4);
-  auto actual = llama2::RmsNorm<float>::Compute(x, weight);
+    x_.resize(4);
+    weight_.resize(4);
+    for (size_t i = 0; i < 4; i++) {
+      x_[i] = dis(gen);
+      weight_[i] = dis(gen);
+    }
+
+    transformer_ = std::make_unique<llama2::Transformer<float>>(kChkPointPath);
+  }
+
+  void TearDown() override {
+    // code here will be called just after the test completes
+    // ok to through exceptions from here if need be
+  }
+
+  std::vector<float> x_;
+  std::vector<float> weight_;
+
+  const std::string kChkPointPath = "stories15M.bin";
+
+  std::unique_ptr<llama2::Transformer<float>> transformer_;
+  std::unique_ptr<llama2::TransformerWeights<float>> weights_;
+};
+
+TEST_F(RmsNormTest, RmsNormTest) {
+  const size_t kSize = 4;
+  std::vector<float> expected_o(kSize);
+  auto actual = llama2::RmsNorm<float>::Compute(x_, weight_);
+  reference::rmsnorm(expected_o.data(), x_.data(), weight_.data(), kSize);
+
+  EXPECT_EQ(actual.size(), expected_o.size());
 }
