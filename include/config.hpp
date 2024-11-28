0e1f6245314abae7ae170ebc8f1c9703dd73f1ce
Heecheol Yang <heecheol.yang@dnotitia.com>
2024-11-28 13:38:32 +0900

    Rename llama2 codes as llama2

--- include/config.hpp
+++ include/config.hpp
@@ -65,4 +65,48 @@ class Config {
   llama_uint32_t kVocabSize;  ///< Size of the vocabulary
   llama_uint32_t kSeqLen;     ///< Maximum sequence length
 };
+
+Config::Config(std::ifstream &config_file) { load_config(config_file); }
+
+llama_uint32_t Config::Dim() const { return kDim; }
+
+llama_uint32_t Config::HiddenDim() const { return kHiddenDim; }
+
+llama_uint32_t Config::NumLayers() const { return kNumLayers; }
+
+llama_uint32_t Config::NumHeads() const { return kNumHeads; }
+
+llama_uint32_t Config::NumKVHeads() const { return kNumKVHeads; }
+
+llama_uint32_t Config::HeadDim() const { return kHeadDim; }
+
+llama_uint32_t Config::KVHeadDim() const { return kKVHeadDim; }
+
+llama_uint32_t Config::KVMul() const { return kKVMul; }
+
+llama_uint32_t Config::VocabSize() const { return kVocabSize; }
+
+llama_uint32_t Config::SeqLen() const { return kSeqLen; }
+
+void Config::load_config(std::ifstream &config_file) {
+  config_file.read(reinterpret_cast<char *>(&kDim), sizeof(kDim));
+  config_file.read(reinterpret_cast<char *>(&kHiddenDim), sizeof(kHiddenDim));
+  config_file.read(reinterpret_cast<char *>(&kNumLayers), sizeof(kNumLayers));
+  config_file.read(reinterpret_cast<char *>(&kNumHeads), sizeof(kNumHeads));
+  config_file.read(reinterpret_cast<char *>(&kNumKVHeads), sizeof(kNumKVHeads));
+  config_file.read(reinterpret_cast<char *>(&kVocabSize), sizeof(kVocabSize));
+  config_file.read(reinterpret_cast<char *>(&kSeqLen), sizeof(kSeqLen));
+
+  kVocabSize = VocabSize();
+  kKVHeadDim = Dim() * NumKVHeads() / NumHeads();
+  kHeadDim = Dim() / NumHeads();
+  kKVMul = NumHeads() / NumKVHeads();
+}
+
+size_t Config::Size() {
+  return sizeof(kDim) + sizeof(kHiddenDim) + sizeof(kNumLayers) +
+         sizeof(kNumHeads) + sizeof(kNumKVHeads) + sizeof(kVocabSize) +
+         sizeof(kSeqLen);
+}
+
 }  // namespace llama
